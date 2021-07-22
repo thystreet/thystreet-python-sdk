5dbc6e36603ce91ac73ea6c5746d19c1c1f4b308
Akash <akash@thystreet.com>
2021-07-22 06:58:16 +0000

    Regenerate client from commit 37db50c of spec repo

---
+++ thystreet/models/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from thystreet.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from thystreet.model.device_status_dto import DeviceStatusDto
+from thystreet.model.device_token_dto import DeviceTokenDto
+from thystreet.model.order_status_dto import OrderStatusDto
